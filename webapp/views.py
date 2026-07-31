c31b6ac9f8ff6eedfab09fbd20ce82621fb5cf11
flohms18 <trouilletflorent@outlook.com>
2026-07-31 18:53:28 +0200

    Add navbar and prepare graph

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,14 @@
+import plotly.graph_objects as go
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    fig = go.Figure(go.Bar(
+        x=[20, 14, 23],
+        y=['giraffes', 'orangutans', 'monkeys'],
+        orientation='h'))
+
+    plot_div = fig.to_html(full_html=False, include_plotlyjs='cdn')
+
+    return render(request, 'index.html', {'plot_div': plot_div})
