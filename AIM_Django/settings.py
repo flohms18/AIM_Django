b82e7e0d48216f484ec79f31450f2f871e60cd22
flohms18 <trouilletflorent@outlook.com>
2026-08-03 00:17:51 +0200

    update change

--- AIM_Django/settings.py
+++ AIM_Django/settings.py
@@ -106,8 +106,15 @@ WSGI_APPLICATION = 'AIM_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
