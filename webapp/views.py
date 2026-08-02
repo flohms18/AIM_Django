991101c4ee658575728b4adf48dfc7c7097c4004
flohms18 <trouilletflorent@outlook.com>
2026-08-02 12:18:33 +0200

    Many addon

--- webapp/views.py
+++ webapp/views.py
@@ -3,12 +3,45 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# TODO: remplacer par les vraies données une fois disponibles.
+TOPICS = [
+    ("Generative AI", 7),
+    ("Data Engineering", 8),
+    ("RAG & Fine-tuning", 9),
+    ("Transformers & NLP", 10),
+    ("Data Governance", 12),
+    ("LLMs", 15),
+]
+
+
 def index(request):
+    labels = [label for label, _ in TOPICS]
+    values = [value for _, value in TOPICS]
+
     fig = go.Figure(go.Bar(
-        x=[20, 14, 23],
-        y=['giraffes', 'orangutans', 'monkeys'],
-        orientation='h'))
+        x=values,
+        y=labels,
+        orientation='h',
+        marker=dict(color='#5c6bff', cornerradius=6),
+        text=values,
+        textposition='outside',
+        hovertemplate='%{y} : %{x}<extra></extra>',
+    ))
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color='#9ca3af', family='ui-sans-serif, system-ui, sans-serif', size=12),
+        margin=dict(l=0, r=30, t=10, b=10),
+        xaxis=dict(visible=False),
+        yaxis=dict(showgrid=False, autorange='reversed', color='#e5e7eb'),
+        bargap=0.45,
+        height=260,
+    )
 
-    plot_div = fig.to_html(full_html=False, include_plotlyjs='cdn')
+    topics_chart = fig.to_html(
+        full_html=False,
+        include_plotlyjs='cdn',
+        config={'displayModeBar': False, 'responsive': True},
+    )
 
-    return render(request, 'index.html', {'plot_div': plot_div})
+    return render(request, 'index.html', {'topics_chart': topics_chart})
